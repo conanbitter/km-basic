e1e6a7ea03a2e77b7296c79bb53b04135b5274a5
Conan Bitter <conan.bitter@gmail.com>
2026-08-15 14:31:56 +0300

    Begin working on lexer

---
+++ include/common.h
@@ -0,0 +1,20 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdint.h>
+
+#if INTPTR_MAX == INT64_MAX
+
+typedef int64_t KmInt;
+typedef double KmFloat;
+
+#elif INTPTR_MAX == INT32_MAX
+
+typedef int32_t KmInt;
+typedef float KmFloat;
+
+#else
+#error "Unknown architecture"
+#endif
+
+#endif
