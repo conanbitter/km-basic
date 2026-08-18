8f94e61a8c986a655f1e061a1bf7f0194019ddad
Conan Bitter <conan.bitter@gmail.com>
2026-08-18 21:37:59 +0300

    Add token debug print

--- src/lexer.c
+++ src/lexer.c
@@ -16,6 +16,75 @@ static bool input_eof;
 static int file_line;
 static int file_col;
 
+#ifdef _DEBUG
+const char* TOKEN_NAMES[] = {
+    "ERROR",
+    "KW_AND",
+    "KW_ANDTHEN",
+    "KW_CASE",
+    "KW_CONST",
+    "KW_CONTINUE",
+    "KW_DECLARE",
+    "KW_DIM",
+    "KW_DO",
+    "KW_ELSE",
+    "KW_ELSEIF",
+    "KW_END",
+    "KW_EXIT",
+    "KW_FOR",
+    "KW_FUNCTION",
+    "KW_GLOBAL",
+    "KW_IF",
+    "KW_INPUT",
+    "KW_IS",
+    "KW_LEN",
+    "KW_LOOP",
+    "KW_MOD",
+    "KW_NEXT",
+    "KW_NOT",
+    "KW_OR",
+    "KW_ORELSE",
+    "KW_PRINT",
+    "KW_REF",
+    "KW_REM",
+    "KW_RETURN",
+    "KW_SELECT",
+    "KW_STATIC",
+    "KW_STEP",
+    "KW_SUB",
+    "KW_THEN",
+    "KW_TO",
+    "KW_UNTIL",
+    "KW_WHILE",
+    "KW_XOR",
+    "NEWLINE",
+    "EOF",
+    "ID",
+    "INTLIT",
+    "FLOATLIT",
+    "STRLIT",
+    "LPAREN '('",
+    "RPAREN ')'",
+    "COMMA ','",
+    "SEMICOLON ';'",
+    "PLUS '+'",
+    "MINUS '-'",
+    "MUL '*'",
+    "DIV '/'",
+    "INTDIV '\\'",
+    "POWER '^'",
+    "CONCAT '&'",
+    "LSHIFT '<<'",
+    "RSHIFT '>>'",
+    "EQ '='",
+    "NEQ '<>'",
+    "GT '>'",
+    "GTEQ '>='",
+    "LS '<'",
+    "LSEQ '<="
+};
+#endif
+
 char curchar;
 
 struct DecTableRow {
@@ -349,6 +418,28 @@ void next_token(char* buffer, size_t buffer_length) {
         break;
 
     default:
+        break;
+    }
+}
+
+void print_token(char* buffer) {
+#ifdef _DEBUG
+    printf("[%d:%d] %s", token.line, token.col, TOKEN_NAMES[token.token_type]);
+#else
+    printf("[%d:%d] %d", token.line, token.col, token.token_type);
+#endif
+    switch (token.token_type) {
+    case TOKEN_INTLIT:
+        printf("= %d", token.int_value);
+        break;
+    case TOKEN_FLOATLIT:
+        printf("= %f", token.float_value);
+        break;
+    case TOKEN_ID:
+        printf("'%.*s'", token.length, buffer);
+        break;
+    case TOKEN_STRLIT:
+        printf("\"%.*s\"", token.length, buffer);
         break;
     }
 }
