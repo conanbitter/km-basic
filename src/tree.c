6eb032a5432c68653c12e39eaeff78f2fc7e8f24
Conan Bitter <conan.bitter@gmail.com>
2026-08-24 22:02:55 +0300

    Improve format on tree debug print

--- src/tree.c
+++ src/tree.c
@@ -3,13 +3,14 @@
 void debug_print_tree(char* start, char* end) {
     TreeNode* _end = (TreeNode*)end;
     TreeNode* cur = (TreeNode*)start;
+    uintptr_t _start = (uintptr_t)start;
     while (cur != _end)
     {
-        printf("%p ", (void*)cur);
+        printf("%4d ", (uintptr_t)cur - _start);
         switch (cur->node_type)
         {
         case NODE_BINOP:
-            printf("binop     %d  %p, %p\n", cur->binop.op, (void*)(cur->binop.left), (void*)(cur->binop.right));
+            printf("binop     %d  [%d], [%d]\n", cur->binop.op, (uintptr_t)(cur->binop.left) - _start, (uintptr_t)(cur->binop.right) - _start);
             break;
 
         case NODE_FLOATLIT:
@@ -21,7 +22,7 @@ void debug_print_tree(char* start, char* end) {
             break;
 
         case NODE_ITOF:
-            printf("itof      %p\n", (void*)(cur->child));
+            printf("itof      [%d]\n", (uintptr_t)(cur->child) - _start);
             break;
         }
         cur++;
